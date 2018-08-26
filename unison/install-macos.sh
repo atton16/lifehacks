1c181b39805feb523543863d5a37d2a4f26c1cdd
Attawit Kittikrairit <atton16@gmail.com>
2018-08-26 17:48:31 +0700

    Update unison/install-macos.sh

--- unison/install-macos.sh
+++ unison/install-macos.sh
@@ -6,6 +6,6 @@
 brew install unison
 
 # Workaround for the missing unison-fsmonitor on MacOS
-sudo pip install watchdog macfsevents
+sudo -H pip install watchdog macfsevents
 curl https://raw.githubusercontent.com/hnsl/unox/master/src/unox/unox.py | sudo tee /usr/local/bin/unison-fsmonitor >/dev/null
 sudo chmod +x /usr/local/bin/unison-fsmonitor
