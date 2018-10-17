8cf2b5797dc1bb392d36b6ee08e5f59675528083
Attawit Kittikrairit <atton16@gmail.com>
2018-10-17 12:05:59 +0700

    Move out rect-ts-boilerplate

--- react/react-ts-boilerplate/src/components/widget-header/widget-header.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { HeaderWidget } from './widget-header';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<HeaderWidget title="" />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- react/react-ts-boilerplate/src/validators/required.ts
+++
@@ -1,8 +0,0 @@
-const required = (value: any) => {
-  if (!value.toString().trim().length) {
-    return false;
-  }
-  return true;
-};
-
-export default required;
